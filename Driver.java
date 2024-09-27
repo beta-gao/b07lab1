488d3b8bc804aea7f5c8886f49844e7ca3529851
beta-gao <yqgbeta711@gmail.com>
2024-09-27 01:01:08 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Driver {
-	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
-	}
+    public static void main(String[] args) {
+        Polynomial p = new Polynomial();
+        System.out.println(p.evaluate(3));
+
+        double [] c1 = {6, 5};
+        int [] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double [] c2 = {-2, -9};
+        int [] e2 = {1, 4};
+        Polynomial p2 = new Polynomial(c2, e2);
+
+        Polynomial s = p1.add(p2);
+        System.out.println("s(0.1) = " + s.evaluate(0.1));
+
+        if (s.hasRoot(1)) {
+            System.out.println("1 is a root of s");
+        } else {
+            System.out.println("1 is not a root of s");
+        }
+
+        Polynomial poly1 = new Polynomial(new double[]{2, 3, 4}, new int[]{2, 0, 1}); // 2x^2 + 3 + 4x
+        Polynomial poly2 = new Polynomial(new double[]{1, 5, 1}, new int[]{2, 1, 0}); // x^2 + 5x + 1
+        Polynomial result = poly1.multiply(poly2);
+		result.printPolynomial();
+        poly1.saveToFile("output_polynomial.txt");
+		poly1.printPolynomial();
+
+        try {
+            File polynomialFile = new File("output_polynomial.txt"); 
+            Polynomial polyFromFile = new Polynomial(polynomialFile);
+            polyFromFile.printPolynomial();  
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Make sure 'polynomial.txt' exists.");
+        }
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,39 +1,137 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Polynomial{
-    double field [];
+    double coefficient [];
+    int exponent [];
 
     public Polynomial(){
-        field = new double[]{0};
+        coefficient = new double[]{0};
+        exponent = new int[]{0};
     }
-    public Polynomial(double[] input){
-        field = new double[input.length];
-        for(int i = 0; i < input.length; i++) {
-        	field[i] = input[i];
+    public Polynomial(double[] input_co, int[] input_ex){
+        coefficient = input_co.clone();
+        exponent = input_ex.clone();
+    }
+    public Polynomial(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String all = scanner.nextLine();
+        scanner.close();
+        String[] terms = all.split("(?=[+-])");
+        double[] new_coefficient = new double[terms.length];
+        int[] new_exponent = new int[terms.length];
+        for(int i = 0; i < terms.length; i++){
+            if(terms[i].contains("x")){
+                String[] term = terms[i].split("x");
+                if(term[0].equals("+")||term[0].equals("")){
+                    new_coefficient[i] = 1;
+                }else if(term[0].equals("-")){
+                    new_coefficient[i] = -1;
+                }else{
+                    new_coefficient[i] = Double.parseDouble(term[0]);
+                }
+                if (term.length > 1) {
+                    new_exponent[i] = Integer.parseInt(term[1]);
+                } else {
+                    new_exponent[i] = 1;
+            }
+        }
+            else{
+                new_coefficient[i] = Double.parseDouble(terms[i]);
+                new_exponent[i] = 0;
+            }
+        }
+        coefficient = new_coefficient;
+        exponent = new_exponent;
+    }
+    
+    public void saveToFile(String name) {
+        StringBuilder poly = new StringBuilder();
+
+        for (int i = 0; i < coefficient.length; i++) {
+            double coeff = coefficient[i];
+            int exp = exponent[i];
+            if (i > 0) {
+                if (coeff > 0) {
+                    poly.append("+");
+                }
+            }
+            if (coeff == 1 && exp != 0) {
+                poly.append("");
+            } else if (coeff == -1 && exp != 0) {
+                poly.append("-");
+            } else {
+                poly.append(coeff);
+            }
+            if (exp != 0) {
+                poly.append("x");
+                if (exp != 1) {
+                    poly.append(exp);
+                }
+            }
+        }
+        try (FileWriter writer = new FileWriter(name)) {
+            writer.write(poly.toString());
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
+
     public Polynomial add(Polynomial other){
-    	int max_length = Math.max(field.length, other.field.length);
-        double[] new_array = new double[max_length];
-        for(int i = 0; i < max_length; i++){
-        	if(i < field.length && i < other.field.length) {
-            new_array[i] = field[i]+other.field[i];
-        	}
-        	else if(i < field.length) {
-        		new_array[i] = field[i];
-        	}
-        	else if(i < other.field.length) {
-        		new_array[i] = other.field[i];
-        	}
+        HashMap<Integer, Double> dictionary = new HashMap<>();
+        for(int i = 0; i < coefficient.length; i++){
+            dictionary.put(exponent[i], coefficient[i]);
         }
-       return new Polynomial(new_array);
+        for(int i = 0; i < other.coefficient.length; i++){
+            dictionary.merge(other.exponent[i], other.coefficient[i], Double::sum);
+        }
+        int[] new_exponent = new int[dictionary.size()];
+        double[] new_coefficient = new double[dictionary.size()];
+        int index = 0;
+        for (Map.Entry<Integer, Double> entry : dictionary.entrySet()) {
+            new_exponent[index] = entry.getKey();
+            new_coefficient[index] = entry.getValue();
+            index++;
+        }
+        return new Polynomial(new_coefficient, new_exponent);
     }
     public double evaluate(double x){
         double result = 0;
-        for(int i = 0; i < field.length; i++){
-            result = result + field[i]*Math.pow(x, i);
+        for(int i = 0; i < coefficient.length; i++){
+            result = result + coefficient[i]*Math.pow(x, exponent[i]);
         }
         return result;
     }
     public boolean hasRoot(double x) {
     	return evaluate(x) == 0;
+    }
+    public Polynomial multiply(Polynomial other){
+        HashMap<Integer, Double> dictionary = new HashMap<>();
+        for(int i = 0; i < coefficient.length; i++){
+            for(int k = 0; k < other.coefficient.length; k++){
+                dictionary.merge(exponent[i]+other.exponent[k], coefficient[i]*other.coefficient[k], Double::sum);
+            }
+        }
+        int[] new_exponent = new int[dictionary.size()];
+        double[] new_coefficient = new double[dictionary.size()];
+        int index = 0;
+        for (Map.Entry<Integer, Double> entry : dictionary.entrySet()) {
+            new_exponent[index] = entry.getKey();
+            new_coefficient[index] = entry.getValue();
+            index++;
+        }
+        return new Polynomial(new_coefficient, new_exponent);
+    }
+    public void printPolynomial() {
+        for (int i = 0; i < coefficient.length; i++) {
+            System.out.print(coefficient[i] + "x" + exponent[i] + " ");
+        }
+        System.out.println();
     }
 }
